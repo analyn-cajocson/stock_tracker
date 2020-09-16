51db58b13a454d440b24181a3a2c64b7854a7ba9
Analyn Cajocson <analyncajocson@ymail.com>
2020-09-16 22:32:05 +0800

    created a class method in stock model for iex client

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,9 @@
 class Stock < ApplicationRecord
+
+  def self.new_lookup(ticket_symbol)
+    client = IEX::Api::Client.new(publishable_token: Rails.application.credentials[:iex_client][:publishable_token],
+                                  secret_token: Rails.application.credentials[:iex_client][:secret_toke],
+                                  endpoint: 'https://sandbox.iexapis.com/v1')
+    client.price(ticket_symbol)
+  end
 end
